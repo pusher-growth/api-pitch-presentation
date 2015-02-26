40766884f25002374ac1e275ec2da92c03da28f6
leggetter <phil@leggetter.co.uk>
2015-02-26 17:52:37 +0000

    Move very basic setup code into init.js

--- js/slides.js
+++ js/slides.js
@@ -1,18 +1,4 @@
 (function() {
-  var slideshow = remark.create( {
-    navigation: {
-      scroll: false,
-    }
-  });
-  
-  var pitcher = CONFIG.getPitcher();
-  jQuery('.pitcher_name').text(pitcher.name);
-  jQuery('.pitcher_title').text(pitcher.title);
-  jQuery('.pitch_app_url').text(CONFIG.pitchAppUrl);
-  jQuery('.prize_description').text(CONFIG.prize);
-})();
-
-(function() {
   Pusher.log = function(msg) {
     console.log(msg);
   };
